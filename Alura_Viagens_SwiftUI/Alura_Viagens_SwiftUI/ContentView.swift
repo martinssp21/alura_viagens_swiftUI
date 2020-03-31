ace7b7ecba10b7efb54f5298acb531fa7b6128a1
Rodrigo Martins <rodrigo.sp19@gmail.com>
2020-03-31 00:46:37 -0300

    Initial commit

---
+++ Alura_Viagens_SwiftUI/Alura_Viagens_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alura_Viagens_SwiftUI
+//
+//  Created by Rodrigo Martins on 31/03/20.
+//  Copyright © 2020 Rodrigo Martins. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
