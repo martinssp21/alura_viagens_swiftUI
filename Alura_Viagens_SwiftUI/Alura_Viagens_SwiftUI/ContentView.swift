254adc7600a64f83dc59ed8286ea76c524c64f18
Rodrigo Martins <rodrigo.sp19@gmail.com>
2020-03-31 17:04:31 -0300

    Formatando text e ajustando view

--- Alura_Viagens_SwiftUI/Alura_Viagens_SwiftUI/ContentView.swift
+++ Alura_Viagens_SwiftUI/Alura_Viagens_SwiftUI/ContentView.swift
@@ -10,7 +10,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        GeometryReader { view in
+            
+            // MARK: - Principal
+            VStack {
+                
+                // MARK: - Header
+                VStack {
+                    Text("alura viagens")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, 50)
+                    
+                    Text("ESPECIAL")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                    
+                    Text("BRASIL")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Black", size: 23))
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 30)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(/*@START_MENU_TOKEN@*/Color.purple/*@END_MENU_TOKEN@*/)
+                .edgesIgnoringSafeArea(.all)
+                
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Atibaia")
+                    Text("Rio de Janeiro")
+                }
+            }
+        }
     }
 }
 
